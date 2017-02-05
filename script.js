d1f3640350ed1a6cd963fe80585254e45fb20ba1
Trista Tian <z27tian@edu.uwaterloo.ca>
2017-02-05 13:07:46 -0500

    Make functions for validating and playing notes

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// Constants.
+var noteDuration = 1.0;
+var noteVolume = 1.0;
+var defaultOctave = 5;
+
+// Octave of the previous note.
+var previousOctave = defaultOctave;
+
+// Audio context for playing notes.
 var audio = new (window.AudioContext || window.webkitAudioContext)()
 
 function playNote(frequency, volume, duration)
@@ -20,10 +29,60 @@ function playNote(frequency, volume, duration)
     o.stop(audio.currentTime + duration);
 }
 
+// Returns true if the note is valid.
+function validateNote(note) {
+	if(note.length > 2 || note.length == 0) {
+		return false;
+	}
+
+	if (note[0] >= "a" && note[0] <= "z") {
+		note[0] = note[0] + ("a" - "A");
+	}
+
+	if(note[0] < "A" || note[0] > "G") {
+		return false;
+	}
+
+	if(note.length ==2 && (note[1] < "0" || note[1] > "8" )) {
+		return false;
+	}
+	else return true;
+}
+
+// Returns 0 for C, 1 for D, ..., 5 for A, 6 for B.
+function letterToPitchIndex(letter) {
+	if (letter >= "A" && letter <= "B") {
+		return letter - "A" + 5;
+	}
+	else return letter - "C";
+}
+
+// Converts the note to a frequency. Assumes letter is uppercase.
+function noteToFrequency(note) {
+	var pitchIndex = letterToPitchIndex(note[0]);
+	var octave = (note.length == 2) ? note[1] - "0" : previousOctave;
+	previousOctave = octave;
+	var i = 7*octave + pitchIndex;
+	return 16.35 * Math.pow (2, i/7);
+
+}
+
 function play() {
 	var melody = document.getElementById("melody").value;
-	//alert("The melody is " + melody + "!");
-	playNote(440, 1, 1);
+	var notes = melody.split(" ");
+
+	// Validate all the notes.
+	for (var i = 0; i < notes.length; i++) {
+		if (!validateNote(notes[i])) {
+			alert("Invalid note " + notes[i] + ". Try again!");
+			return;
+		}
+	}
+
+	// Play the notes.
+	for (var i = 0; i < notes.length; i++) {
+		playNote(noteToFrequency(notes[i]), noteVolume, noteDuration);
+	}
 }
 
 function playWithHarmony() {
